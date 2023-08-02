b3c47b62e90938ff5243c44e28841792e8f1da4e
actuallyyao <bingxu.yao@hotmail.com>
2023-08-03 00:44:19 +0800

    home

--- js/cursor.js
+++ js/cursor.js
@@ -39,7 +39,7 @@ class Cursor {
                 this.pt.push(el[i].outerHTML);
 
         document.body.appendChild((this.scr = document.createElement("style")));
-        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' fill='rgb(80, 80, 80)' opacity='1'/></svg>") 4 4, auto}`;
+        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' fill='rgb(70, 70, 70)' opacity='1'/></svg>") 4 4, auto}`;
     }
 
     refresh() {
